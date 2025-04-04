b3713ab583e3b50a2a44e6f0740815e464893ab9
Harry121199 <harrythe454@gmail.com>
2025-04-05 01:13:45 +0530

    updated user in model

--- backend/Mentra/src/main/java/com/EdTech/Mentra/model/User.java
+++ backend/Mentra/src/main/java/com/EdTech/Mentra/model/User.java
@@ -1,4 +1,14 @@
 package com.EdTech.Mentra.model;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
 public class User {
+    private int userId;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
 }
