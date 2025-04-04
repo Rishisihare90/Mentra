ff1dca7843697bd3648ca2cc99a43dc7328cd2fd
Harry121199 <harrythe454@gmail.com>
2025-04-05 01:23:41 +0530

    updated user in model

--- backend/Mentra/src/main/java/com/EdTech/Mentra/model/User.java
+++ backend/Mentra/src/main/java/com/EdTech/Mentra/model/User.java
@@ -1,12 +1,23 @@
 package com.EdTech.Mentra.model;
 
+import jakarta.persistence.*;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+@Entity
 @Data
 @NoArgsConstructor
+@Table(name = "users",
+        uniqueConstraints = {
+        @UniqueConstraint(columnNames = "email")
+        })
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
     private int userId;
+
+    @Not
     private String firstName;
     private String lastName;
     private String email;
