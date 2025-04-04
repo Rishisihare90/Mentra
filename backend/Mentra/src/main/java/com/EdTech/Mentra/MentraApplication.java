46658d2681a0a0d65ae8b470b8a7783eb9a6bf41
anubhavraiii <realar99@gmail.com>
2025-04-04 23:40:30 +0530

    Add backend and frontend folders

---
+++ backend/Mentra/src/main/java/com/EdTech/Mentra/MentraApplication.java
@@ -0,0 +1,13 @@
+package com.EdTech.Mentra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MentraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MentraApplication.class, args);
+	}
+
+}
---
+++ backend/Mentra/src/test/java/com/EdTech/Mentra/MentraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.EdTech.Mentra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MentraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
